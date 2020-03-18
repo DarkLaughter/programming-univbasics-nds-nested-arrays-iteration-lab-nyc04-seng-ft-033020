745e3d55347e600c39c8a34bef675f2265ebb90d
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-18 02:15:29 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -50,6 +50,7 @@ def total_even_pairs(src)
     if ((src[counter][0] % 2 == 0) and (src[counter][1]) % 2 == 0)
         total += src[counter][0] + src[counter][1]
     end
+    counter += 1
   end
   total
 end
